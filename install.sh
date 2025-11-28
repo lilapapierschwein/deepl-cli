9f8f43fd65cd04e6c45167181be727b8881dcd29
lilapapierschwein <kai.elsaesser@posteo.de>
2025-11-28 05:30:37 +0100

    fix: installer

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-install () {
-    local install_path=/usr/local/bin/
-    
-    go build main.go
-    mv main deepl
-    ln -s $PWD/deepl $install_path
+install_deepl_cli () {
+    local install_path=/usr/local/bin
+
+    go build $PWD/deepl.go
+    sudo ln -s $PWD/deepl $install_path
+
+    echo "application successfully installed at $install_path/deepl"
+    echo "run \`deepl --help\` for info on usage and shell completions"
 }
 
-install
+install_deepl_cli
